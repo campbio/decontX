7c415ee30e896b151a1888673fc3dab1136732ee
Yuan <76789185+yuan-yin-truly@users.noreply.github.com>
2023-02-16 18:33:56 -0500

    Working version of decontPro

---
+++ R/decontX-package.R
@@ -0,0 +1,16 @@
+#' The 'decontX' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name decontX-package
+#' @aliases decontX
+#' @useDynLib decontX, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.7. https://mc-stan.org
+#'
+NULL
