c22982c7ed621cc6d1cb752c15e73ca1823cd98a
Yuan <76789185+yuan-yin-truly@users.noreply.github.com>
2023-02-22 17:23:45 -0500

    add unit test for decontPro

--- tests/testthat/test-decontPro.R
+++ tests/testthat/test-decontPro.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(decontX)
+context("Testing decontPro function")
+
+test_that(desc = "Testing decontPro on count matrix", {
+  set.seed(42)
+  counts <- matrix(sample(1:10,
+                          1000,
+                          replace = TRUE),
+                   ncol = 10)
+
+  k <- c(1,1,2,2,2,3,3,4,4,4)
+
+  out <- decontPro(counts, k, 1e-2, 1e-2)
+
+  # Sum decomposed matrices
+  matsum = out$decontaminated_counts +
+    out$ambient_counts +
+    out$background_counts
+
+  expect_equal(matsum, counts)
 })
